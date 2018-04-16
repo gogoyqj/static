53457376be022a486cb931e77c3d979d7b116fe6
yang <tmp@tmpdeMacBook-Pro.local>
2018-04-16 15:46:10 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const serve = require('koa-static');
+const Koa = require('koa');
+const app = new Koa();
+ 
+app.use(serve(__dirname + '../../asset_do_not_modify'));
+app.use(serve(__dirname + '../../mail_do_not_modify'));
+ 
+app.listen(3003);
